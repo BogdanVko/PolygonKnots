3d767b284f550a8e57e5d41b463acc54b158b19a
Николай Санников <kolyambosunny@gmail.com>
2019-10-03 00:51:47 -0400

    Implemented gaussian random value generation for vector values. Done this using numpy, which becomes one of the dependencies

--- Polygon_research.py
+++ Polygon_research.py
@@ -1,6 +1,6 @@
 #  Created by Bogdan Vasilchenko on 02/Oct/2019
 
-
+import numpy
 """  Generating Quaternion Vectors.
 
         Step One -> Get 2 arrays of Complex Vectors [(Real,Imaginary),
@@ -27,19 +27,21 @@ def random_number():
     number = 1  # STUB for now. Should be a random number from Gaussian Distribution
     return number
 
+def generate_random_vectors(size):
+    """
+    :param size: length of the complex vector arrays (ex.: size = 5 => there will be 5 random complex vectors)
+    :return: returns a tuple of 2 arrays of complex vectors, each array has a provided length and 2 values for each vector: real and imaginary parts
+    mean, standard deviation, and number of values per vector are hardcoded to be specific for this project
+    """
+    shape_to_generate=(size,2)
+    mean =0
+    stdDiv = 0.1
+    array_of_complex_vectors = numpy.random.normal(mean, stdDiv, shape_to_generate)
+    return array_of_complex_vectors
 
 # Fill 2 arrays of complex vectors. @param number of vectors to make; @fill the array of vectors
-for i in range(initial_amount_of_complex_vectors):
-    # Tuple represents a vector (#, #i)
-    vector = (random_number(), random_number())
-
-    array_of_complex_vectors1.append(vector)
-
-for i in range(initial_amount_of_complex_vectors):
-    # Tuple represents a vector (#, #i)
-    vector = (random_number(), random_number())
-    array_of_complex_vectors2.append(vector)
-
+array_of_complex_vectors1 = generate_random_vectors(initial_amount_of_complex_vectors)
+array_of_complex_vectors2 = generate_random_vectors(initial_amount_of_complex_vectors)
 
 # Perform Gram-schmidt to create an array of
 def gram_schmidt(array_of_vectors1, array_of_vectors2):
